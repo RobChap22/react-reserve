6c58a580881499c1dfa3a176812f59ad546e638d
Rob Chapman <autosexual@gmail.com>
2020-09-16 21:01:13 +0900

    baseUrl for production and dev environments

--- pages/product.js
+++ pages/product.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import ProductSummary from '../components/Product/ProductSummary';
 import ProductAttributes from '../components/Product/ProductAttributes';
+import baseUrl from '../utils/baseUrl';
 
 
 function Product({ product }) {
@@ -14,7 +15,7 @@ function Product({ product }) {
 
 Product.getInitialProps = async ({ query }) => {
   // the query here comes from the url
-  const url = `http://localhost:3000/api/product?_id=${query._id}`;
+  const url = `${baseUrl}/api/product?_id=${query._id}`;
   // const payload = { params: {query._id} };
   const response = await axios.get(url);
   return { product: response.data };
