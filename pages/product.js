fa8835dfbaff511b43dc4d52cd9ea3086a6173d3
Rob Chapman <autosexual@gmail.com>
2020-09-16 20:01:43 +0900

    product show backend done

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,15 @@
-function Product() {
+import axios from 'axios';
+
+function Product({ product }) {
   return <>product</>;
+}
+
+function getInitialProps = async ({ query }) => {
+  // the query here comes from the url
+  const url = `http://localhost:3000/api/product?_id=${query._id}`;
+  // const payload = { params: {query._id} };
+  const response = await axios.get(url);
+  return { product: response.data };
 }
 
 export default Product;
