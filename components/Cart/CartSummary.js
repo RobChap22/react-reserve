a46a37d912a25d20e0202c309352943714476e6c
Rob Chapman <autosexual@gmail.com>
2020-09-17 22:26:40 +0900

    cart main done for logged in and guest

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,13 @@
+import { Segment, Button, Divider } from 'semantic-ui-react';
+
+
+
+
 function CartSummary() {
-  return <>CartSummary</>;
+  return <>
+    <Divider />
+    <Segment></Segment>
+  </>;
 }
 
 export default CartSummary;
