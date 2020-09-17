1ef3d18c78c411d23a55667c26ed745cbd348b70
Rob Chapman <autosexual@gmail.com>
2020-09-17 22:29:47 +0900

    main cart structure done time for a beer

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -6,7 +6,15 @@ import { Segment, Button, Divider } from 'semantic-ui-react';
 function CartSummary() {
   return <>
     <Divider />
-    <Segment></Segment>
+    <Segment clearing size="large">
+      <strong>Subtotal:</strong> $0.00
+      <Button
+        icon="cart"
+        color="teal"
+        floated="right"
+        content="Checkout"
+      />
+    </Segment>
   </>;
 }
 
