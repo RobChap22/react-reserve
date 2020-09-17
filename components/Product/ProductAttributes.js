28a3e1e30a919d4dc03668b95419acce4e05eacf
Rob Chapman <autosexual@gmail.com>
2020-09-17 14:58:51 +0900

    useState up and running

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { Header, Button, Modal } from 'semantic-ui-react';
 
 function ProductAttributes({ description }) {
+  const [modal, setModal] = React.useState(false);
+
   return <>
     <Header as='h3'>About this product</Header>
     <p>{description}</p>
@@ -8,10 +11,26 @@ function ProductAttributes({ description }) {
       icon='trash alternate outline'
       color='red'
       content='Delete Product'
+      onClick={() => setModal(true)}
     />
-    <Modal open={true} dimmer='blurring'>
-      <Modal.Header>
-      </Modal.Header>
+
+    <Modal open={modal} dimmer='blurring'>
+      <Modal.Header>Confirm Delete</Modal.Header>
+      <Modal.Content>
+        <p>Are you sure you want to delete this item?</p>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          content='Cancel'
+          onClick={() => setModal(false)}
+        />
+        <Button
+          negative
+          icon="trash"
+          labelPosition="right"
+          content="Delete"
+        />
+      </Modal.Actions>
     </Modal>
   </>;
 }
