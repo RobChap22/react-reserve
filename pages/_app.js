b5d1805e8c8c89600cab82630e4d8ad015605d7c
Rob Chapman <autosexual@gmail.com>
2020-09-22 20:12:59 +0900

    account api forbidden

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,39 @@
 import App from "next/app";
+import axios from 'axios';
+import { parseCookies } from 'nookies';
 import Layout from '../components/_App/Layout';
+import { redirectUser } from '../utils/auth';
+import baseUrl from '../utils/baseUrl';
+
 
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
+    const { token } = parseCookies(ctx);
+
     let pageProps = {};
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
+
+    if (!token) {
+      const isProtectedRoute = cxt.pathname === '/account' || cxt.pathname === '/create'
+      if (isProtectedRoute) {
+        redirectUser(ctx, '/login');
+      }
+    } else {
+      try {
+        const url = `${baseUrl}/api/account`;
+        const payload = { headers: { Authorization: token } };
+        const response = await axios.get(url, payload);
+        const user = response.data;
+        pageProps.user = user;
+      } catch (error) {
+        console.error("Error getting current user", error);
+      }
+    }
+
     return { pageProps };
   }
 
@@ -16,7 +41,7 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
+      <Layout {...pageProps} >
         <Component {...pageProps} />
       </Layout>
     );
---
+++ pages/api/account.js
@@ -0,0 +1,23 @@
+import User from '../../models/User';
+import connectDb from '../../utils/connectDb';
+import jwt from 'jsonwebtoken';
+
+connectDb();
+
+export default async (req, res) => {
+  if (!req.headers.authorization) {
+    res.status(401).send("No authorization token.")
+  }
+
+  try {
+    const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: userId });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    res.status(403).send('Invalid token');
+  }
+}
