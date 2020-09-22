592ff4dcb8eb10cc3cb5fef1e3f0ca4c5209c72c
Rob Chapman <autosexual@gmail.com>
2020-09-22 15:32:27 +0900

    validations added to signin

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,6 +2,8 @@ import connectDb from '../../utils/connectDb';
 import User from "../../models/User";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import isEmail from 'validator/lib/isEmail';
+import isLength from 'validator/lib/isLength';
 
 
 connectDb();
@@ -9,6 +11,14 @@ connectDb();
 export default async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    if (!isLength(name, { min: 3, max: 10})) {
+      return res.status(422).send('Name must be 3-10 characters long');
+    } else if (!isLength(password, { min: 6 })) {
+      return res.status(422).send('Password must be at least 6 characters long');
+    } else if (!isEmail(email)) {
+      return res.status(422).send('Not a valid email');
+    }
+
     const user = await User.findOne({ email });
     if (user) {
       res.status(422).send(`User already exists with email ${email}!`);
