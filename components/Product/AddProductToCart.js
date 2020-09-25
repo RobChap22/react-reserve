743455b2b7f91032501d50713f0c9652346bf94a
Rob Chapman <autosexual@gmail.com>
2020-09-25 18:13:57 +0900

    add product to cart backend structure done

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,16 +1,40 @@
 import { Input } from "semantic-ui-react";
+import React from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl';
+import cookie from 'js-cookie';
 
-function AddProductToCart() {
+
+function AddProductToCart({ user, productId }) {
+  const [quantity, setQuantity] = React.useState(1);
+  const router = useRouter();
+
+  async function handleAddProductToCart() {
+    const url = `${baseUrl}/api/cart`;
+    const payload = { quantity, productId };
+    const token = cookie.get('token');
+    const headers = { headers: { Authorization: token } };
+    await axios.put(url, payload, headers);
+  }
+
   return (
     <Input
       type="number"
       min="1"
       placeholder="Quantity"
-      value={1}
-      action={{
+      value={quantity}
+      onChange={e => setQuantity(Number(e.target.value))}
+      action={user ? {
         color: "orange",
         content: "Add to Cart",
-        icon: "plus cart"
+        icon: "plus cart",
+        onClick: handleAddProductToCart
+      } : {
+        color: "blue",
+        content: "Sign up to purchase item",
+        icon: "signup",
+        onClick: () => router.push('/signup')
       }}
     />
   );
