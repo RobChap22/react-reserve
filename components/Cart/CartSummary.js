b7816e8ea1470c322a6f32355c801f7b96e30a50
Rob Chapman <autosexual@gmail.com>
2020-09-25 22:06:38 +0900

    cart functionality running

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,12 +1,25 @@
 import { Button, Segment, Divider } from "semantic-ui-react";
+import React from 'react';
+import calculateCartTotal from '../../utils/calculateCartTotal';
 
-function CartSummary() {
+function CartSummary({ products }) {
+  const [isCartEmpty, setCartEmpty] = React.useState(false);
+  const [cartAmount, setCartAmount] = React.useState(0);
+  const [stripeAmount, setStripeAmount] = React.useState(0);
+
+  React.useEffect(() => {
+    const {cartTotal, stripeTotal} = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
+    setCartEmpty(products.length === 0);
+  }, [products])
+
   return (
     <>
       <Divider />
       <Segment clearing size="large">
-        <strong>Sub total:</strong> $0.00
-        <Button icon="cart" color="teal" floated="right" content="Checkout" />
+        <strong>Sub total:</strong> ${cartAmount}
+        <Button disabled={isCartEmpty} icon="cart" color="teal" floated="right" content="Checkout" />
       </Segment>
     </>
   );
--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -3,19 +3,39 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import baseUrl from '../../utils/baseUrl';
+import catchErrors from '../../utils/catchErrors';
 import cookie from 'js-cookie';
 
 
 function AddProductToCart({ user, productId }) {
   const [quantity, setQuantity] = React.useState(1);
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
   const router = useRouter();
 
+  React.useEffect(() => {
+    let timeout;
+    if (success) {
+      timeout = setTimeout(() => setSuccess(false), 3000)
+    }
+    return () => {clearTimeout(timeout)};
+  })
+
+
   async function handleAddProductToCart() {
-    const url = `${baseUrl}/api/cart`;
-    const payload = { quantity, productId };
-    const token = cookie.get('token');
-    const headers = { headers: { Authorization: token } };
-    await axios.put(url, payload, headers);
+    try{
+      setLoading(true);
+      const url = `${baseUrl}/api/cart`;
+      const payload = { quantity, productId };
+      const token = cookie.get('token');
+      const headers = { headers: { Authorization: token } };
+      await axios.put(url, payload, headers);
+      setSuccess(true);
+    } catch (error) {
+      catchErrors(error, window.alert);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -25,10 +45,17 @@ function AddProductToCart({ user, productId }) {
       placeholder="Quantity"
       value={quantity}
       onChange={e => setQuantity(Number(e.target.value))}
-      action={user ? {
+      action={ user && success ? {
+        color: "blue",
+        content: "Item added!",
+        disabled: true,
+        icon: "plus cart"
+      } : user ? {
         color: "orange",
         content: "Add to Cart",
         icon: "plus cart",
+        loading,
+        disabled: loading,
         onClick: handleAddProductToCart
       } : {
         color: "blue",
