4768a2f0ee8b4c9e613762c06602d94a2c134fca
Rob Chapman <autosexual@gmail.com>
2020-09-28 22:57:54 +0900

    rollback on pagination

--- components/Index/ProductPagination.js
+++ components/Index/ProductPagination.js
@@ -1,5 +1,18 @@
-function ProductPagination() {
-  return <>ProductPagination</>;
+import { Container, Pagination } from 'semantic-ui-react';
+
+
+function ProductPagination({ totalPages }) {
+  return (
+    <Container textAlign="center" style={{margin: '2em'}} >
+      <Pagination
+        defaultActivePage={1}
+        totalPages={totalPages}
+        onPageChange={(event, data) => {
+          e
+        }}
+      />
+    </Container>
+  );
 }
 
 export default ProductPagination;
