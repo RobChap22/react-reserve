2b7a20c4bcbc8b8b5d673180e95efbfbd5b06223
Rob Chapman <autosexual@gmail.com>
2020-09-29 00:30:37 +0900

    deployment baseUrl try two

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://react-reserve-nine.vercel.app/"
+    ? "https://react-reserve-e263y5cjc.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
