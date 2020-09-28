cec08572b358d197659da70156270d9a1df85fc8
Rob Chapman <autosexual@gmail.com>
2020-09-28 23:37:56 +0900

    account page done

--- components/Account/AccountOrders.js
+++ components/Account/AccountOrders.js
@@ -1,5 +1,26 @@
-function AccountOrders() {
-  return <>AccountOrders</>;
+import { Header, Icon, Segment, Button } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+
+function AccountOrders({ orders }) {
+  const router = useRouter()
+
+  return <>
+    <Header as="h2" icon>
+      <Icon name="folder open"/>
+      Order History
+    </Header>
+    <Segment inverted tertiary color="grey" textAlign="center">
+      <Header icon>
+        <Icon name="copy outline" />
+        No orders yet.
+      </Header>
+      <div>
+        <Button onClick={() => router.push('/')} color="orange" >
+          View Products
+        </Button>
+      </div>
+    </Segment>
+  </>;
 }
 
 export default AccountOrders;
--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,27 @@
-function Account() {
-  return <>account</>;
+import AccountHeader from '../components/Account/AccountHeader';
+import AccountOrders from '../components/Account/AccountOrders';
+import { parseCookies } from 'nookies';
+import baseUrl from '../utils/baseUrl';
+import axios from 'axios';
+
+
+function Account({ user, orders }) {
+  return <>
+    <AccountHeader {...user} />
+    <AccountOrders orders={orders} />
+  </>;
+}
+
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { orders: [] };
+  }
+
+  const url = `${baseUrl}`
+  const payload = {headers: { Authorization: token }};
+
+  return { orders: [] };
 }
 
 export default Account;
